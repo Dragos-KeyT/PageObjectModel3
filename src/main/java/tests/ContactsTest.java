738b1b0e1b5477107dea7d94a0442ace3c0f03e0
Dragos Tanta <dragos.tanta@gmail.com>
2024-06-04 17:01:08 +0300

    curs 25 Select class

--- src/main/java/tests/ContactsTest.java
+++ src/main/java/tests/ContactsTest.java
@@ -10,14 +10,14 @@ public class ContactsTest extends BaseTest {
 	public void sendMessageFromContactFormTest() {
 	
 		app.click(app.menu.contactsLink);
-		
+		//1
 		app.sendKeys(app.contacts.nameField, "Harry Potter");
 		app.sendKeys(app.contacts.emailField, "harry@howgarths.com");
 		app.sendKeys(app.contacts.subjectField, "Addmision");
 		app.sendKeys(app.contacts.msgField, "Can I join?");
 		app.click(app.contacts.submitBtn);
 		
-		
+		//2
 		app.contacts.sendMessage("Ion", "ion@ion.com", "Salut", "Ce mai faci?");
 	
 	}
