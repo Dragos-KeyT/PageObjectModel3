048a56ff11ad8af0ddcd01aba625a872a172601b
Dragos Tanta <dragos.tanta@gmail.com>
2024-06-11 17:57:08 +0300

    curs28 actionClass

--- src/main/java/selenium/utils/SeleniumWrappers.java
+++ src/main/java/selenium/utils/SeleniumWrappers.java
@@ -5,8 +5,10 @@ import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.TestException;
 
 public class SeleniumWrappers extends BaseTest{
 
@@ -37,6 +39,25 @@ public class SeleniumWrappers extends BaseTest{
 		
 		driver.findElement(locator).sendKeys(text);
 		
+	}
+	
+	
+	public void hoverElement(By locator) {
+		
+		try {
+			WebElement element = driver.findElement(locator);
+			Actions action = new Actions(driver);
+			action.moveToElement(element).perform();
+			
+			
+		}catch(Exception e) {
+			new TestException(e.getMessage());
+		}	
+	}
+	
+	
+	public WebElement getWebElement(By locator) {
+		return driver.findElement(locator);
 	}
 	
 	
